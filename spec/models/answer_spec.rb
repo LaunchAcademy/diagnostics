1c4d55734cf8b1cfb80bf0e5700a6812d666ee86
Richard Davis <rdavis.bacs@gmail.com>
2015-01-16 16:33:43 -0500

    model specs

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Answer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Answer, type: :model do
+  it { should have_many(:questions) }
+  it { should validate_presence_of(:content) }
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'valid_attribute'
+require 'shoulda/matchers'
 
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
