1285972674499c315ab14eaadd92388f0abf2dfd
Richard Davis <rdavis.bacs@gmail.com>
2015-01-16 17:51:40 -0500

    question specs

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,11 @@ class Question < ActiveRecord::Base
   has_many :question_answers
   has_many :answers,
     through: :question_answers
-    
+
   validates :query, presence: true
+
+  def correct(answer)
+    qa = QuestionAnswer.where(question: self, answer: answer)
+    qa.correct?
+  end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,5 +1,14 @@
 FactoryGirl.define do
   factory :question do
     query "What language is the Rails framework written in?"
+
+    factory :question_with_answers do
+      after(:create) do
+        create(:answer, content: "Ruby", correct: true)
+        create(:answer, content: "SQL")
+        create(:answer, content: "Javascript")
+        create(:answer, content: "Python")
+      end
+    end
   end
 end
