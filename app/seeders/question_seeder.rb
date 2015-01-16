c8edef0e58ab87f39a258054b664b8f910362dfc
Richard Davis <rdavis.bacs@gmail.com>
2015-01-16 17:50:26 -0500

    question seeder implemented

--- app/seeders/question_seeder.rb
+++ app/seeders/question_seeder.rb
@@ -3,22 +3,28 @@ class QuestionSeeder
 
   def initialize(yaml_file)
     @yaml_file = yaml_file
-    load_questions
+    load_questions_data
   end
 
   def seed!
     questions_data.each do |query, answers_data|
-      Question.find_or_create_by(query: query)
+      question = Question.find_or_create_by!(query: query)
 
       answers_data.each do |content|
-        answer = Answer.find_or_create_by(content: content)
-        answer.save!
+        answer = Answer.find_or_create_by!(content: content)
+
+        qa_params = {
+          question: question,
+          answer: answer,
+          correct: answer == answers_data.first
+        }
+        question_answer = QuestionAnswer.find_or_create_by!(qa_params)
       end
     end
   end
 
   private
-  def load_questions
+  def load_questions_data
     File.open(@yaml_file, "r") do |file|
       @questions_data = YAML.load(file)
     end
