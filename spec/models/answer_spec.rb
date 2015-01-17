602e6c3f8a0edc43a11d2c506bc62b5a01a2adfe
Richard Davis <rdavis.bacs@gmail.com>
2015-01-17 13:43:37 -0500

    test for 'correct' boolean field

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,5 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should have_many(:questions) }
+
   it { should validate_presence_of(:content) }
+
+  it { should have_valid(:correct).when(true, false) }
+  it { should_not have_valid(:correct).when(nil, "") }
 end
