e1f624dacd671fd693487df20f8393d468d7f960
Richard Davis <rdavis.bacs@gmail.com>
2015-01-19 18:21:04 -0500

    refactor test

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -3,15 +3,17 @@ require "rails_helper"
 feature "user authentication" do
   let(:user) { FactoryGirl.create(:user) }
 
-  scenario "user signs in" do
+  before(:each) do
     mock_omni_auth_for(user)
+  end
+
+  scenario "user signs in" do
     visit root_path
     click_on "Sign in with GitHub"
     expect(page).to have_content("Welcome, #{user.name}!")
   end
 
   scenario "user signs out" do
-    mock_omni_auth_for(user)
     visit root_path
     click_on "Sign in with GitHub"
     click_on "Sign out"
