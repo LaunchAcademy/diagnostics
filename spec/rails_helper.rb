1857eb4bb49f0ea42a46b1d454bc1c3f8d237d75
Richard Davis <rdavis.bacs@gmail.com>
2015-01-21 16:45:17 -0500

    removed commented mock helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,9 +17,4 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-
-  # clean up OmniAuth mocks after each example
-  # config.after(:example) do
-  #   OmniAuth.config.mock_auth[:github] = nil
-  # end
 end
