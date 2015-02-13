9b2a8d04baeae9bd1154206c5cd230a474e80412
cmkoller <cmkoller01@gmail.com>
2015-02-13 15:31:30 -0500

    Deleted time validation

--- app/models/answer_submission.rb
+++ app/models/answer_submission.rb
@@ -12,7 +12,6 @@ class AnswerSubmission < ActiveRecord::Base
     message: "You already answered that question." }
 
   validate :ip_must_be_local
-  validate :time_of_answer_must_be_valid
 
   def self.on_date(date)
     where('DATE(created_at) = ?', date)
@@ -22,29 +21,8 @@ class AnswerSubmission < ActiveRecord::Base
     ip == ENV['LAUNCH_ACADEMY_IP']
   end
 
-  def valid_time?
-    current_time = Time.now
-    weekends = %w(Saturday Sunday)
-    if weekends.include?(current_time.strftime("%A"))
-      return false
-    end
-
-    start_time = Time.zone.parse("9:00")
-    end_time = Time.zone.parse("17:00")
-
-    if !((start_time <= current_time) && (current_time <= end_time))
-      return false
-    end
-    true
-  end
-
   def ip_must_be_local
     errors.add(:ip, "must be local to Launch Academy to answer a quiz.") if
       !valid_location?
-  end
-
-  def time_of_answer_must_be_valid
-    errors.add(:created_at, "must be during the work day") if
-      !valid_time?
   end
 end
--- spec/models/answer_submission_spec.rb
+++ spec/models/answer_submission_spec.rb
@@ -24,31 +24,4 @@ RSpec.describe AnswerSubmission, type: :model do
     expect(answer.errors.full_messages).to include("Ip must be local to Launch Academy to answer a quiz.")
   end
 
-  describe "#valid_time" do
-    it "should return true if creation time is during workday" do
-      Timecop.freeze(Time.local(2015, 2, 10, 9, 0, 0)) do  # 9AM Tuesday
-        answer = FactoryGirl.create(:answer_submission)
-
-        expect(answer.valid_time?).to eq(true)
-      end
-    end
-  end
-
-  it "should raise an error if creation time is during evening" do
-    Timecop.freeze(Time.local(2015, 2, 10, 22, 0, 0)) do  # 10PM Tuesday
-      answer = FactoryGirl.build(:answer_submission)
-      answer.save
-
-      expect(answer.errors.full_messages).to include("Created at must be during the work day")
-    end
-  end
-
-  it "should raise an error if creation time is during weekend" do
-    Timecop.freeze(Time.local(2015, 2, 14, 9, 0, 0)) do  # 9AM Saturday
-      answer = FactoryGirl.build(:answer_submission)
-      answer.save
-
-      expect(answer.errors.full_messages).to include("Created at must be during the work day")
-    end
-  end
 end
