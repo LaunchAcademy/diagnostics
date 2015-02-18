66f520af178223173e33fb266c70c540c17224ae
David Tengdin <dreamingfurther@gmail.com>
2015-02-18 17:16:16 -0500

    Change Question answer page to accommodate longer questions

--- spec/features/user_starts_a_quiz_spec.rb
+++ spec/features/user_starts_a_quiz_spec.rb
@@ -16,8 +16,8 @@ feature "user starts a quiz" do
     ActionDispatch::Request.any_instance.stub(:remote_ip).and_return(ENV['LAUNCH_ACADEMY_IP'])
     sign_in(user)
     visit quiz_path(quiz)
-    click_on first_question.answers.first.content
-    click_on second_question.answers.first.content
+    within(all('.new_answer_submission').first) { click_on 'X' }
+    within(all('.new_answer_submission').first) { click_on 'X' }
     expect(page).to have_content("#{quiz.name} complete.")
   end
 end
